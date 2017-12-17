4ea82c65df7a80ebb4111e48cd25b1928492f7e8
George Xanthakis <kompiouterakias@hotmail.com>
2017-12-17 18:37:05 +0200

    minor change

--- usefulcode/bemainaf.c
+++ usefulcode/bemainaf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 __attribute__((constructor)) static void _bar() { printf("bar\n"); }
+
 int main(){
 	printf("HELLO\n");
 	printf("HELLO\n");
 
 }
+
 __attribute__((destructor)) static void _bar2() { printf("bar\n"); }
