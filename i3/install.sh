f26dc8a27d88b744401021b9e2934952005d0685
George Xanthakis <kompiouterakias@hotmail.com>
2017-12-22 22:40:12 +0000

    updated with install scripts

---
+++ i3/install.sh
@@ -0,0 +1,1 @@
+sudo pacman -S numlockx network-manager-applet
--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-if [ -d ~/.config ] then
+if [ ! -d ~/.config ] then
 	mkdir ~/.config
 	mkdir ~/.dir_colors
 fi
+./i3/install.sh
+./urxvt/install.sh
 cp zsh/zshrc ~/.zshrc
 cp -r i3 ~/.config
 cp -r ranger ~/.config
 cp urxvt/dircolors ~/.dir_colors/dircolors
 cp urxvt/Xresources ~/.Xresources
+xrdb merge ~/.Xresources
