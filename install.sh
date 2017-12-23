b376d8c60474ff8bffbd76f4fda10669bb6683fc
George Xanthakis <kompiouterakias@hotmail.com>
2017-12-23 16:43:42 +0200

    Added gnome-term solarized

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-if [ ! -d ~/.config ] then
+if [ ! -d ~/.config ]; then
 	mkdir ~/.config
+fi
+
+if [ ! -d ~/.dir_colors ]; then
 	mkdir ~/.dir_colors
 fi
+
 ./i3/install.sh
 ./urxvt/install.sh
 cp zsh/zshrc ~/.zshrc
@@ -10,3 +14,8 @@ cp -r ranger ~/.config
 cp urxvt/dircolors ~/.dir_colors/dircolors
 cp urxvt/Xresources ~/.Xresources
 xrdb merge ~/.Xresources
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git gterm-solarized
+cd gterm-solarized
+./install.sh
+cd ..
+
