58988f031cc7760a983ff29d0533e80a2350f775
George Xanthakis <kompiouterakias@hotmail.com>
2018-04-27 01:31:06 +0300

    Found workaround to use the i3bar

--- i3/launch_polybar.sh
+++
@@ -1,9 +0,0 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    echo $m
-    MONITOR=$m polybar --reload example &
-    echo $MONITOR
-  done
-else
-	polybar --reload example &
-fi
