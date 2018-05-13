25aa4e0cbd216de71f693c8cb12ffc6e67cfc0fe
George Xanthakis <kompiouterakias@hotmail.com>
2018-05-13 13:48:25 +0300

    Updated installation script and added touchpad extra functionality

--- i3/install.sh
+++ i3/install.sh
@@ -1,5 +1,7 @@
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1D1F0DC78F173680
+yaourt -S bauerbill
 sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro
-yaourt ls-icons cquery polybar bear-git
+yaourt -S ls-icons cquery bear-git
 sudo pip install git+https://github.com/Sarcasm/compdb.git#egg=compdb
 sudo pip install 'python-language-server[all]' pyls-mypy pyls-isort
 
