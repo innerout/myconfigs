6945d5e84cf9fbaf7f681057690bf164586c978d
George Xanthakis <kompiouterakias@hotmail.com>
2018-06-28 19:37:56 +0300

    Update

--- i3/install.sh
+++ i3/install.sh
@@ -1,7 +1,7 @@
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1D1F0DC78F173680
-yaourt -S bauerbill
-sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro
-yaourt -S ls-icons cquery bear-git
+trizen -S bauerbill
+sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro zeal ncdu 
+trizen -S ls-icons bear-git
 sudo pip install git+https://github.com/Sarcasm/compdb.git#egg=compdb
 sudo pip install 'python-language-server[all]' pyls-mypy pyls-isort
 
