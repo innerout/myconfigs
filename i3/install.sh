e133ff5070d074889c6c294b47b114da817ed51b
George Xanthakis <kompiouterakias@hotmail.com>
2018-07-14 19:13:27 +0300

    Update

--- i3/install.sh
+++ i3/install.sh
@@ -1,7 +1,7 @@
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1D1F0DC78F173680
 trizen -S bauerbill
-sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro zeal ncdu 
+sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro zeal ncdu ibus isync msmtp
 trizen -S ls-icons bear-git
-sudo pip install git+https://github.com/Sarcasm/compdb.git#egg=compdb
-sudo pip install 'python-language-server[all]' pyls-mypy pyls-isort
+sudo pip install -r requirements.txt
+sudo npm install dictionary-el dictionary-el-polyton dictionary-en-us dictionary-en-gb
 
--- urxvt/installurxvtranger.sh
+++ urxvt/installurxvtranger.sh
@@ -1,1 +1,1 @@
-yaourt -S urxvt-tabbedex bspwm siji ttf-unifont i3ipc-glib libmpdclient polybar urxvtconfig rxvt-unicode-pixbuf ranger 
+yaourt -S urxvt-tabbedex siji ttf-unifont i3ipc-glib libmpdclient polybar urxvtconfig rxvt-unicode-pixbuf ranger 
