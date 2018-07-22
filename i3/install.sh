1ecc8bd9a937ed4906b1a02f878aeac99048889b
George Xanthakis <kompiouterakias@hotmail.com>
2018-07-22 18:19:07 +0300

    Update

--- i3/install.sh
+++ i3/install.sh
@@ -1,7 +1,5 @@
-gpg --recv-keys --keyserver hkp://pgp.mit.edu 1D1F0DC78F173680
 trizen -S bauerbill
-sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro zeal ncdu ibus isync msmtp
-trizen -S ls-icons bear-git
+sudo pacman -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro zeal ncdu ibus isync msmtp autojump notify-osd zsh-syntax-highlighting otf-font-awesome
+trizen -S ls-icons bear-git xcursor-breeze
 sudo pip install -r requirements.txt
 sudo npm install dictionary-el dictionary-el-polyton dictionary-en-us dictionary-en-gb
-
