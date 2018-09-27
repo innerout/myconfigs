bd4da03c24d2d3ed4d87cad523b80b17127578cb
George Xanthakis <kompiouterakias@hotmail.com>
2018-09-27 15:29:26 +0300

    fzf

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,6 @@
 trizen -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro
 trizen -S zeal ncdu ibus isync msmtp autojump notify-osd zsh-syntax-highlighting otf-font-awesome linux-zen
-trizen -S linux-zen-headers linux-lts linux-lts-headers otf-overpass
+trizen -S linux-zen-headers linux-lts linux-lts-headers otf-overpass fzf llvm clang
 
 trizen -S ls-icons bear-git xcursor-breeze arc-x-icons-theme ccat
 
