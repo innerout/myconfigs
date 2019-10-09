c3d551925447f7883abbb6bfa91059037ad92881
George Xanthakis <kompiouterakias@hotmail.com>
2019-10-09 15:52:31 +0300

    Add backup script and default apps for gnome

---
+++ backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ~/.local/share/applications/mimeapps.list ~/gitfolders/myconfigs/zsh/
+cp /etc/pacman.conf ~/gitfolders/myconfigs/zsh/
+cp /etc/makepkg.conf ~/gitfolders/myconfigs/zsh/
