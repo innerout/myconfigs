c41aad5113dc232d4a71a0352638bc962fc525bc
George Xanthakis <kompiouterakias@hotmail.com>
2020-03-21 02:07:25 +0200

    Make tray icons appear properly

--- zsh/gsettings.sh
+++ zsh/gsettings.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 gsettings set org.gnome.gnome-flashback desktop false
 gsettings set org.gnome.gnome-flashback root-background true
+gsettings set org.gnome.gnome-flashback status-notifier-watcher false
