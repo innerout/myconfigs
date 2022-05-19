622c8a76df027ac546dd894beaefe357988d7e32
George Xanthakis <kompiouterakias@hotmail.com>
2022-05-19 13:59:54 +0300

    Update i3 dependencies

--- i3/install.sh
+++ i3/install.sh
@@ -1,9 +1,2 @@
-trizen -S numlockx network-manager-applet arc-gtk-theme volumeicon redshift cscope pandoc adobe-source-code-pro
-trizen -S zeal ncdu ibus isync msmtp autojump notify-osd zsh-syntax-highlighting otf-font-awesome linux-zen
-trizen -S linux-zen-headers linux-lts linux-lts-headers otf-overpass fzf llvm clang
-
-trizen -S ls-icons bear-git xcursor-breeze arc-x-icons-theme ccat
-
-sudo pip install -r requirements.txt
-sudo npm install dictionary-el dictionary-el-polyton dictionary-en-us dictionary-en-gb
-sudo npm install --global fkill-cli
+#!/bin/bash
+yay -S networkmanager-pptp network-manager-applet numlockx xcursor-breeze arc-x-icons-theme ncdu ibus isync msmtp
--- i3/kernel_dependencies.sh
+++
@@ -1,2 +0,0 @@
-trizen -S wd719x-firmware aic94xx-firmware
-sudo mkinitcpio -p linux-zen
