046eb71d50c5f93bcc104baaccf59e81dc846bed
George Xanthakis <kompiouterakias@hotmail.com>
2022-05-19 14:00:10 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo cp zsh/pacman.conf /etc/pacman.conf
 
 if [ ! -d ~/.config ]; then
 	mkdir ~/.config
+	mkdir ~/.config/i3
 fi
 
 if [ ! -d ~/.dir_colors ]; then
@@ -15,11 +16,5 @@ fi
 cp zsh/zshrc ~/.zshrc
 cp zsh/dircolors ~/.dir_colors/
 cp i3/config ~/.config/i3/config
-
-# Gnome terminal solarized
-git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git gterm-solarized
-cd gterm-solarized
-./install.sh
-cd ..
 
 echo "Dont forget to install better fonts from here https://gist.github.com/cryzed/e002e7057435f02cc7894b9e748c5671"
