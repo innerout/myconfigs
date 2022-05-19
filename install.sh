530789e4782c121ddb6973833ff517af99b08af8
George Xanthakis <kompiouterakias@hotmail.com>
2022-05-19 14:02:03 +0300

    Install terminal configs

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ sudo cp zsh/pacman.conf /etc/pacman.conf
 if [ ! -d ~/.config ]; then
 	mkdir ~/.config
 	mkdir ~/.config/i3
+	mkdir ~/.config/alacritty
+	mkdir ~/.config/kitty
 fi
 
 if [ ! -d ~/.dir_colors ]; then
@@ -16,5 +18,7 @@ fi
 cp zsh/zshrc ~/.zshrc
 cp zsh/dircolors ~/.dir_colors/
 cp i3/config ~/.config/i3/config
+cp terminal/alacritty/* ~/.config/alacritty/
+cp terminal/kitty/* ~/.config/kitty/
 
 echo "Dont forget to install better fonts from here https://gist.github.com/cryzed/e002e7057435f02cc7894b9e748c5671"
