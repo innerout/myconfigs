7dc747da998807452d857171ecac84c0f19fcf92
George Xanthakis <kompiouterakias@hotmail.com>
2022-05-27 15:00:52 +0300

    Update zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp ~/lscolors.sh ~/.lscolors.sh
-yay -S autojump lsd fkill fd ripgrep the_silver_searcher hstr-git trash-cli
+yay -S autojump lsd fkill fd ripgrep the_silver_searcher hstr-git trash-cli lscolors-git
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
