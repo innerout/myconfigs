571e3df281f0313ee18f0ed04d7dd045c3877180
George Xanthakis <kompiouterakias@hotmail.com>
2022-10-23 00:08:37 +0300

    Add latest ohmyzsh config

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,3 +3,4 @@ yay -S autojump lsd fkill fd ripgrep the_silver_searcher hstr-git trash-cli lsco
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/alias-tips
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
